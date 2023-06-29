07e6a4b6f1c7d5a85f995d4959bca3f2aab30b75
Florent <florent.hagard@gmail.com>
2023-06-29 14:55:07 +0200

    Update Workflow + add Square

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app     = express();
 const port    = 80;
+const square = require("./lib/square")
 
 app.disable("x-powered-by")
 
 app.get('/', (req, res) => {
     res.send("hello world");
 });
+
+app.get('/square/:nb', (req, res) => {
+	const nb = req.params.nb
+	res.write(square(parseInt(nb)))
+	res.end()
+});
+
 
 app.listen(port,() => {
     console.log(`Server is running on port ${port}`);
