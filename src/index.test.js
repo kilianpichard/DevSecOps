98118403c0c0ed04ce51a69cfda5c9ee37bc561a
Kilian Pichard <kilian.pichard@botify.com>
2023-06-30 14:20:24 +0200

    remove home test

--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,13 @@
-const request = require('supertest');
-const { app, closeServer } = require('./index');
+const request = require("supertest");
+const { app, closeServer } = require("./index");
 
-describe('Server is running', () => {
-    afterAll(() => {
-        closeServer();
-    });
+describe("Server is running", () => {
+  afterAll(() => {
+    closeServer();
+  });
 
-    it('Should return 200', async () => {
-        const res = await request(app).get('/');
-        expect(res.status).toBe(200);
-    });
-
-    it('Should return "hello world"', async () => {
-        const res = await request(app).get('/');
-        expect(res.text).toBe('hello world');
-    });
+  it("Should return 200", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
+  });
 });
